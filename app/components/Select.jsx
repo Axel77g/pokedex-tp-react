3924bfef9fcc78cc734afc881e07d3a1aae1df01
Axel <axel@reality-academy.fr>
2025-01-09 11:10:59 +0100

    lazy loading

--- app/components/Select.jsx
+++ app/components/Select.jsx
@@ -1,6 +1,42 @@
-export function Select({multiple, options}){
+import {useEffect, useState} from "react";
+
+export function Select({multiple, options, placeholder, onChange = null}){
     const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState([]);
+
+    useEffect(()=>{
+        if(onChange){
+            onChange(selected)
+        }
+    },[selected])
+
+    function handleOpen(){
+        setOpen(!open);
+    }
+
+    function handleSelect(option){
+        if(multiple){
+            setSelected([...selected, option]);
+        }else{
+            setSelected([option]);
+            setOpen(false);
+        }
+    }
+
+    const label = placeholder || 'Select an option';
     return <div>
-        <div className={'activitor'}></div>
+        <div className={'activitor'} onClick={handleOpen}>
+            {label}
+        </div>
+
+        {open && <div className={'selectContent'}>
+            <div className={'selectOptions'}>
+                {options.map((option, i) => {
+                    return <div key={'option-'+i} onClick={()=>handleSelect(option)}>{option.label}</div>
+                })}
+            </div>
+        </div>}
+
+
     </div>
 }
